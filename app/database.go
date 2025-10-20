f559380f2ad57c1c86207f0d6aa6d5ebf28e2b74
Soe-Hok-Gie <fadilspiegel@gmail.com>
2025-10-20 14:58:48 +0700

    fist commit

---
+++ app/database.go
@@ -0,0 +1,16 @@
+package app
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func NewDB(userDB, passDB, hostDB, portDB, nameDB string) *sql.DB {
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", userDB, passDB, hostDB, portDB, nameDB)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
---
+++ main.go
@@ -0,0 +1,28 @@
+package portal
+
+import (
+	"fmt"
+	"log"
+	"medsos/app"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	//setENV
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	userDB := os.Getenv("DB_USER")
+	passDB := os.Getenv("DB_PASS")
+	hostDB := os.Getenv("DB_HOST")
+	portDB := os.Getenv("DB_PORT")
+	nameDB := os.Getenv("DB_NAME")
+
+	fmt.Println("dsn:", userDB, passDB, hostDB, portDB, nameDB)
+
+	//setDB
+	db := app.NewDB(userDB, passDB, hostDB, portDB, nameDB)
+}

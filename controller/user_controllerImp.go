4d794f4c9298091af664ede741e80b8f01873812
Soe-Hok-Gie <fadilspiegel@gmail.com>
2025-10-22 01:21:02 +0700

    modifikasi

--- controller/user_controllerImp.go
+++ controller/user_controllerImp.go
@@ -1,12 +1,39 @@
 package controller
 
 import (
+	"encoding/json"
+	"medsos/helper"
+	"medsos/model/web"
+	"medsos/service"
 	"net/http"
 )
 
 type userControllerImp struct {
+	userService service.UserService
 }
 
-func (controller *userControllerImp) Create(writer http.ResponseWriter, request *http.Request) UserController {
-	return &userControllerImp
+func (controller *userControllerImp) Create(writer http.ResponseWriter, request *http.Request) {
+	//membaca request body
+	decoder := json.NewDecoder(request.Body)
+	//mengembalikan result, result diambil dari model web
+	result := web.UserCreateRequest{}
+	err := decoder.Decode(&result)
+	helper.PanicIfError(err)
+
+	//memanggil service dan mengembalikan response
+	response := controller.userService.Create(request.Context(), result)
+
+	//membuat standart respose
+	webResponse := web.Response{
+		Code:   http.StatusCreated,
+		Status: http.StatusText(http.StatusCreated),
+		Data:   response,
+	}
+	writer.Header().Add("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusCreated)
+	//proses mengubah data menjadi format lain
+	encoder := json.NewEncoder(writer)
+	err = encoder.Encode(webResponse)
+	helper.PanicIfError(err)
+
 }

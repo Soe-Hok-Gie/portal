16341409ff1867594ff417dd726882d1a033e8cb
Soe-Hok-Gie <fadilspiegel@gmail.com>
2025-10-28 02:39:33 +0700

    membuat sebuah func polimerisme agar bisa diinjek codenya

--- repository/post_repositoryImp.go
+++ repository/post_repositoryImp.go
@@ -11,12 +11,16 @@ type postRepositoryImp struct {
 	DB *sql.DB
 }
 
+func NewPostRepository(DB *sql.DB) PostRepository {
+	return &postRepositoryImp{DB: DB}
+}
+
 func (repository *postRepositoryImp) Save(ctx context.Context, post domain.Post) domain.Post {
 	tx, err := repository.DB.Begin()
 	helper.PanicIfError(err)
 	defer helper.CommitOrRollBack(tx)
 
-	script := "INSERT INTO post (id,user_id, title, contents) VALUES (?,?,?,?)"
+	script := "INSERT INTO post (id, user_id, title, content) VALUES (?,?,?,?)"
 	result, err := tx.ExecContext(ctx, script, post.Id, post.User_Id, post.Title, post.Content)
 	helper.PanicIfError(err)
 

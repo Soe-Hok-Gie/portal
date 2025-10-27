415414a3a46569d8a92f449a1810e5aa73b5cefe
Soe-Hok-Gie <fadilspiegel@gmail.com>
2025-10-27 22:15:31 +0700

    membuat DB transaktion

--- repository/post_repositoryImp.go
+++ repository/post_repositoryImp.go
@@ -2,12 +2,18 @@ package repository
 
 import (
 	"context"
+	"database/sql"
+	"medsos/helper"
 	"medsos/model/domain"
 )
 
 type postRepositoryImp struct {
+	DB *sql.DB
 }
 
 func (repository *postRepositoryImp) Save(ctx context.Context, post domain.Post) domain.Post {
+	tx, err := repository.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollBack(tx)
 
 }

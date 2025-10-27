4f69b3083e697fb706c2c4687660aaffc9f9f14e
Soe-Hok-Gie <fadilspiegel@gmail.com>
2025-10-28 05:13:19 +0700

    membuat sebuah variabel untuk menampung isi dari domain post

--- service/post_service_Imp.go
+++ service/post_service_Imp.go
@@ -2,10 +2,19 @@ package service
 
 import (
 	"context"
+	"medsos/model/domain"
 	"medsos/model/web"
 )
 
 type postServiceImp struct {
 }
 
-func (service postServiceImp) Create(ctx context.Context, request web.PostCreateRequest) web.PostCreateRequest
+func (service postServiceImp) Create(ctx context.Context, request web.PostCreateRequest) web.PostCreateRequest {
+
+	post := domain.Post{
+		User_Id:  request.User_Id,
+		Title:    request.Title,
+		Content:  request.Content,
+		CreateAt: request.CreateAt,
+	}
+}

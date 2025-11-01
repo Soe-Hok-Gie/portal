0e851bd5ead0a341a6ebcfa06cb2d04d0bd61770
Soe-Hok-Gie <fadilspiegel@gmail.com>
2025-11-01 09:57:30 +0700

    membuat sebuah var untuk menampung webResponse

--- controller/post_controllerImp.go
+++ controller/post_controllerImp.go
@@ -102,4 +102,9 @@ func (controller *postControllerImp) FindById(writer http.ResponseWriter, reques
 func (controller *postControllerImp) FindAll(writer http.ResponseWriter, request *http.Request) {
 	postRenponses := controller.postService.FindAll(request.Context())
 
+	webResponse := web.Response{
+		Code:   http.StatusOK,
+		Status: "Ok",
+		Data:   postRenponses,
+	}
 }

97e20894bff04d1890148e6b87e0a30eb5b7a129
Soe-Hok-Gie <fadilspiegel@gmail.com>
2025-11-12 11:56:55 +0700

    menambahkan ?parseTime=true untuk sinkronisasi time.time

--- app/database.go
+++ app/database.go
@@ -7,7 +7,7 @@ import (
 
 func NewDB(userDB, passDB, hostDB, portDB, nameDB string) *sql.DB {
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", userDB, passDB, hostDB, portDB, nameDB)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", userDB, passDB, hostDB, portDB, nameDB)
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		panic(err)

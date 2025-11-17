0dcaa9e6cec996e8c746f63d5c5de1cf5b8960ba
Soe-Hok-Gie <fadilspiegel@gmail.com>
2025-11-17 14:02:01 +0700

    create func InternalServerError

--- exception/error_handler.go
+++ exception/error_handler.go
@@ -4,4 +4,9 @@ import "net/http"
 
 func ErrorHandler(writer http.ResponseWriter, request *http.Request, err interface{}) {
 
+	InternalServerError(writer, request, err)
+}
+
+func InternalServerError(writer http.ResponseWriter, request *http.Request, err interface{}) {
+
 }

92fb97b2043f7fd88e1f310f569670b8c7487f46
Soe-Hok-Gie <fadilspiegel@gmail.com>
2025-11-17 15:44:54 +0700

    create defer func,looping&jalankan serveHttp

--- middleware/recovery.go
+++ middleware/recovery.go
@@ -1,6 +1,9 @@
 package middleware
 
-import "net/http"
+import (
+	"medsos/exception"
+	"net/http"
+)
 
 // Ini adalah middleware.
 // Middleware selalu menerima next, yaitu handler berikutnya (route yang sebenarnya).
@@ -10,6 +13,15 @@ func Recovery(Next http.Handler) http.Handler {
 		// middleware ini membungkus handler berikutnya dengan fungsi tambahan.
 		// Jadi sebelum masuk ke handler asli, kode di dalam sini dijalankan.
 
+		// defer = jalankan kode ini setelah fungsi selesai.
+		defer func() {
+			//looping
+			if rec := recover(); rec != nil {
+				exception.ErrorHandler(w, r, rec)
+			}
+
+		}()
+		Next.ServeHTTP(w, r) //Ini menjalankan handler berikutnya (route asli yang dibuat).
 	})
 
 }

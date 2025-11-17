5e02485d3b0a8bff711821d7e96f2d781e1801e1
Soe-Hok-Gie <fadilspiegel@gmail.com>
2025-11-17 14:56:32 +0700

    create webresponse&Encode

--- exception/error_handler.go
+++ exception/error_handler.go
@@ -1,6 +1,10 @@
 package exception
 
-import "net/http"
+import (
+	"encoding/json"
+	"medsos/model/web"
+	"net/http"
+)
 
 func ErrorHandler(writer http.ResponseWriter, request *http.Request, err interface{}) {
 
@@ -11,4 +15,13 @@ func InternalServerError(writer http.ResponseWriter, request *http.Request, err 
 	writer.Header().Set("Content-Type", "application/json")
 	writer.WriteHeader(http.StatusInternalServerError)
 
+	webResponse := web.Response{
+		Code:   http.StatusInternalServerError,
+		Status: "INTERNAL SERVER ERROR",
+		Data:   err,
+	}
+	writer.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(writer)
+	encoder.Encode(webResponse)
+	// helper.WriteToResponseBody(writer, webResponse)//buat sebuah func dihelper untuk refactor decode and decode
 }

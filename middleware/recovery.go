2f77c6668066ffbcc10c4a651edd0cb2be2cdb4e
Soe-Hok-Gie <fadilspiegel@gmail.com>
2025-11-17 15:36:26 +0700

    return handlefunc

--- middleware/recovery.go
+++ middleware/recovery.go
@@ -6,5 +6,10 @@ import "net/http"
 // Middleware selalu menerima next, yaitu handler berikutnya (route yang sebenarnya).
 // Dan harus mengembalikan http.Handler.
 func Recovery(Next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// middleware ini membungkus handler berikutnya dengan fungsi tambahan.
+		// Jadi sebelum masuk ke handler asli, kode di dalam sini dijalankan.
+
+	})
 
 }

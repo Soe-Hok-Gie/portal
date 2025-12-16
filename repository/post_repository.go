8e45a572a24705eb8b5baf130ca4ab6ca386f28c
Soe-Hok-Gie <fadilspiegel@gmail.com>
2025-12-16 16:56:47 +0700

    Modifikasi error Repo untuk endpoint update

--- repository/post_repository.go
+++ repository/post_repository.go
@@ -7,7 +7,7 @@ import (
 
 type PostRepository interface {
 	Save(ctx context.Context, post domain.Post) (domain.Post, error)
-	Update(ctx context.Context, post domain.Post) domain.Post
+	Update(ctx context.Context, post domain.Post) (domain.Post, error)
 	FindById(ctx context.Context, postId int) (domain.UserPost, error)
 	FindAll(ctx context.Context, filter domain.PostFilter) []domain.Post //tambahkan filter di parameter 2
 	Delete(ctx context.Context, id int)
--- repository/post_repositoryImp.go
+++ repository/post_repositoryImp.go
@@ -36,29 +36,21 @@ func (repository *postRepositoryImp) Save(ctx context.Context, post domain.Post)
 
 }
 
-// tx, err := repository.DB.Begin()
-// helper.PanicIfError(err)
-// defer helper.CommitOrRollBack(tx)
-
-// script := "INSERT INTO post (id, user_id, title, content) VALUES (?,?,?,?)"
-// result, err := tx.ExecContext(ctx, script, post.Id, post.User_Id, post.Title, post.Content)
-// helper.PanicIfError(err)
-// id, err := result.LastInsertId()
-// helper.PanicIfError(err)
-
-// post.Id = int(id)
-// return post
-
-func (repository *postRepositoryImp) Update(ctx context.Context, post domain.Post) domain.Post {
+func (repository *postRepositoryImp) Update(ctx context.Context, post domain.Post) (domain.Post, error) {
 	tx, err := repository.DB.Begin()
 	helper.PanicIfError(err)
 	defer helper.CommitOrRollBack(tx)
 
-	script := "UPDATE post SET user_id=?, title=?,content=? WHERE id=?"
+	// script := "UPDATE post SET user_id=?, title=?,content=? WHERE id=?"
+	// if _, err := tx.ExecContext(ctx, script, post.User_Id, post.Title, post.Content, post.Id); err != nil {
+	// 	panic(err)
+	// }
+	// return post
+	script := "UPDATE post SET user_id=?, title=?, content=? WHERE id=?"
 	if _, err := tx.ExecContext(ctx, script, post.User_Id, post.Title, post.Content, post.Id); err != nil {
-		panic(err)
 	}
-	return post
+	return post, err
+
 }
 func (repository *postRepositoryImp) FindById(ctx context.Context, postId int) (domain.UserPost, error) {
 	var post domain.UserPost
